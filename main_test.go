71c39d5d7204bdbbfaa3baba3bf4286c316a17a6
Amjad Jibon <amjad.jibon@gmail.com>
2023-04-15 10:17:55 +0600

    simple golang hello server

---
+++ main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func TestIndex(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+
+	Index(w, r, nil)
+
+	if w.Code != 200 {
+		t.Errorf("Index() = %d; want 200", w.Code)
+	}
+
+	if w.Body.String() != "Welcome!\n" {
+		t.Errorf("Index() = %s; want Welcome!\n", w.Body.String())
+	}
+}
+
+func TestHello(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/hello/gopher", nil)
+	p := httprouter.Params{httprouter.Param{Key: "name", Value: "gopher"}}
+
+	Hello(w, r, p)
+
+	if w.Code != 200 {
+		t.Errorf("Hello() = %d; want 200", w.Code)
+	}
+
+	if w.Body.String() != "Hello, gopher!\n" {
+		t.Errorf("Hello() = %s; want Hello, gopher!\n", w.Body.String())
+	}
+}
